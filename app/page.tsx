efd38a2cc4825e54a7b4fc3ee66e1393d1526826
altrono <hbotema16@gmail.com>
2024-03-14 10:06:35 +0200

    implemented client side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,50 @@
-import Image from "next/image";
+"use client";
 import { Hero, SearchBar, CustomFilter, CarCard, ShowMore } from "./components";
 import { HomeProps} from "@/types"
 
 import { fetchCars } from "@/utils";
 import { fuels, yearsOfProduction } from "@/constants";
+import { useEffect, useState } from "react";
 
-export default async  function Home({ searchParams }: HomeProps) {
-  const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || '',
-    year: searchParams.year || 2022,
-    fuel: searchParams.fuel || '',
-    limit: searchParams.limit || 10,
-    model: searchParams.model || '',
-    
-  });
+export default  function Home() {
+  const [allCars, setAllCars] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // search states
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  // filter states
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2023);
+
+  // filter states
+  const [limit, setLimit] = useState(10);
+
+  const getCars = async () => {
+    setLoading(true);
+
+    try {
+      const result = await fetchCars({
+        manufacturer: manufacturer || '',
+        year: year || 2022,
+        fuel: fuel || '',
+        limit: limit || 10,
+        model: model || '',
+      });
+  
+      setAllCars(result);
+    } catch(error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(()=> {
+    getCars();
+  }, [fuel, year, limit, manufacturer, model])
+
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   console.log(allCars)
@@ -39,13 +70,13 @@ export default async  function Home({ searchParams }: HomeProps) {
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car, index) => (
-                <CarCard car={car} key={`${car.city_mpg}${car.displacement}-${index}`} />
+                <CarCard car={car} key={`${index}`} />
               ))}
             </div>
 
             <ShowMore
-              pageNumber={(searchParams.limit || 10) / 10}
-              isNext={(searchParams.limit || 10) > allCars.length}
+              pageNumber={(limit || 10) / 10}
+              isNext={(limit || 10) > allCars.length}
             />
           </section>
         ) : (
